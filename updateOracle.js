bdea17a97074e2b849a7affc5e2300c2305920db
Albert <github@albert.sh>
2020-05-11 03:28:24 +0200

    lint

--- updateOracle.js
+++ updateOracle.js
@@ -1,69 +1,47 @@
-const https = require("https")
-const { default: Neon, api, wallet, sc } = require("@cityofzion/neon-js")
-const PRIVATE_KEY = process.env["PRIVATE_KEY"]
+const {
+  default: Neon, api, wallet,
+} = require('@cityofzion/neon-js');
+const getJSON = require('./getJSON');
 
-const CONTRACT_SCRIPTHASH = process.env["CONTRACT_SCRIPTHASH"]
+const { PRIVATE_KEY } = process.env;
 
-const apiProvider = new api.neoscan.instance("MainNet")
-const account = new wallet.Account(PRIVATE_KEY)
+const { CONTRACT_SCRIPTHASH } = process.env;
 
-module.exports = updateOracle
+const apiProvider = new api.neoscan.instance('MainNet');
+const account = new wallet.Account(PRIVATE_KEY);
 
-async function getPrice(){
-	const coingecko = (await getJSON("https://api.coingecko.com/api/v3/simple/price?ids=neo&vs_currencies=bnb")).neo.bnb
-	const messari = (await getJSON("https://data.messari.io/api/v1/assets/bnb/metrics/market-data")).data.market_data.price_usd / (await getJSON("https://data.messari.io/api/v1/assets/neo/metrics/market-data")).data.market_data.price_usd
-	const binance = (await getJSON("https://api.binance.com/api/v3/ticker/24hr?symbol=BNBBTC")).weightedAvgPrice/(await getJSON("https://api.binance.com/api/v3/ticker/24hr?symbol=NEOBTC")).weightedAvgPrice // The USDT pairs have higher volume than the BTC ones but given that everythig else is based on USDT I decided to go with BTC here, also this is a 24hr average price, while the other prices are spot
-	const hitbtc = Number((await getJSON("https://api.hitbtc.com/api/2/public/ticker/NEOUSD")).last)/Number((await getJSON("https://api.hitbtc.com/api/2/public/ticker/BNBUSD")).last)
-	const bitfinex_gateio = (await getJSON("https://api-pub.bitfinex.com/v2/ticker/tNEOUSD"))[6]/(await getJSON("https://data.gateio.life/api2/1/ticker/bnb_usdt")).last
+async function getPrice() {
+  const coingecko = (await getJSON('https://api.coingecko.com/api/v3/simple/price?ids=neo&vs_currencies=bnb')).neo.bnb;
+  const messari = (await getJSON('https://data.messari.io/api/v1/assets/bnb/metrics/market-data')).data.market_data.price_usd / (await getJSON('https://data.messari.io/api/v1/assets/neo/metrics/market-data')).data.market_data.price_usd;
+  const binance = (await getJSON('https://api.binance.com/api/v3/ticker/24hr?symbol=BNBBTC')).weightedAvgPrice / (await getJSON('https://api.binance.com/api/v3/ticker/24hr?symbol=NEOBTC')).weightedAvgPrice; // The USDT pairs have higher volume than the BTC ones but given that everythig else is based on USDT I decided to go with BTC here, also this is a 24hr average price, while the other prices are spot
+  const hitbtc = Number((await getJSON('https://api.hitbtc.com/api/2/public/ticker/NEOUSD')).last) / Number((await getJSON('https://api.hitbtc.com/api/2/public/ticker/BNBUSD')).last);
+  const bitfinexGateio = (await getJSON('https://api-pub.bitfinex.com/v2/ticker/tNEOUSD'))[6] / (await getJSON('https://data.gateio.life/api2/1/ticker/bnb_usdt')).last;
 
-	let medianPrice = [coingecko, messari, binance, hitbtc, bitfinex_gateio].sort()[2]
+  const medianPrice = [coingecko, messari, binance, hitbtc, bitfinexGateio].sort()[2];
 
-	return Math.round(medianPrice*(10**8)) // Convert to BigInt, assume NEP5 decimals = 8
+  return Math.round(medianPrice * (10 ** 8)); // Convert to BigInt, assume NEP5 decimals = 8
 }
 
-async function updateOracle(){
-  const price = await getPrice()
+async function updateOracle() {
+  const price = await getPrice();
 
-	const sb = Neon.create.scriptBuilder()
-	// Your contract script hash, function name and parameters
-	sb.emitAppCall(CONTRACT_SCRIPTHASH, "updatePrice", [medianPrice])
+  const sb = Neon.create.scriptBuilder();
+  // Your contract script hash, function name and parameters
+  sb.emitAppCall(CONTRACT_SCRIPTHASH, 'updatePrice', [price]);
 
-	// Returns a hexstring
-	const script = sb.str
+  // Returns a hexstring
+  const script = sb.str;
 
-	const config = {
-		api: apiProvider, // Network
-		account: account, // Your Account
-		script: script, // The Smart Contract invocation script
-		gas: 0, // Optional, system fee
-		fees: 0 // Optional, network fee
-	}
+  const config = {
+    api: apiProvider, // Network
+    account, // Your Account
+    script, // The Smart Contract invocation script
+    gas: 0, // Optional, system fee
+    fees: 0, // Optional, network fee
+  };
 
-	// Neon API
-	await Neon.doInvoke(config)
+  // Neon API
+  await Neon.doInvoke(config);
 }
 
-// Basic implementation of simple fetch()
-function getJSON(url){
-	return new Promise((resolve, reject) => {
-		let output = ''
-		https.get(url, (res) => {
-			res.setEncoding('utf8')
-
-			res.on('data', (chunk) => {
-				output += chunk
-			})
-
-			res.on('end', () => {
-				let obj = JSON.parse(output)
-				if(res.statusCode != 200){
-					reject("status: "+ res.statusCode)
-				} else {
-					resolve(obj)
-				}
-			})
-		}).on('error', (err) => {
-			reject('error: ' + err.message)
-		})
-	})
-}
+module.exports = updateOracle;
