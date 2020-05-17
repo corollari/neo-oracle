c82df728c3edc9de321bb61a62126152c4e44910
Albert <github@albert.sh>
2020-05-17 20:06:11 +0200

    Add test

--- updateOracle.js
+++ updateOracle.js
@@ -12,16 +12,20 @@ async function getJSON(url) {
   return fetch(url).then((res) => res.json());
 }
 
-async function getPrice(ticker) {
+async function getBNBGASPrice() {
   const coingecko = (await getJSON('https://api.coingecko.com/api/v3/simple/price?ids=gas&vs_currencies=bnb')).gas.bnb;
   const messari = (await getJSON('https://data.messari.io/api/v1/assets/gas/metrics/market-data')).data.market_data.price_usd / (await getJSON('https://data.messari.io/api/v1/assets/bnb/metrics/market-data')).data.market_data.price_usd;
-  const binance = (await getJSON('https://api.binance.com/api/v3/ticker/24hr?symbol=GASBTC')).weightedAvgPrice / (await getJSON('https://api.binance.com/api/v3/ticker/24hr?symbol=BNBBTC')).weightedAvgPrice; // The USDT pairs have higher volume than the BTC ones but given that everythig else is based on USDT I decided to go with BTC here, also this is a 24hr average price, while the other prices are spot, again just trying to balance things out
+  const binance = (await getJSON('https://api.binance.com/api/v3/ticker/24hr?symbol=GASBTC')).lastPrice / (await getJSON('https://api.binance.com/api/v3/ticker/24hr?symbol=BNBBTC')).lastPrice; // The USDT pairs have higher volume than the BTC ones but given that everythig else is based on USDT I decided to go with BTC here
   const hitbtc = Number((await getJSON('https://api.hitbtc.com/api/2/public/ticker/GASUSD')).last) / Number((await getJSON('https://api.hitbtc.com/api/2/public/ticker/BNBUSD')).last);
   const gateio = (await getJSON('https://data.gateio.life/api2/1/ticker/gas_usdt')).last / (await getJSON('https://data.gateio.life/api2/1/ticker/bnb_usdt')).last;
+  return [coingecko, messari, binance, hitbtc, gateio];
+}
 
-  const medianPriceBNBGAS = [coingecko, messari, binance, hitbtc, gateio].sort()[2];
+async function getPrice(ticker) {
+  const BNBGASPrices = await getBNBGASPrice();
+  const medianPriceBNBGAS = BNBGASPrices.sort()[2];
 
-  const tickerPrice = Number((await getJSON(`https://dex-european.binance.org/api/v1/ticker/24hr?symbol=${ticker}`))[0].weightedAvgPrice);
+  const tickerPrice = Number((await getJSON(`https://dex-european.binance.org/api/v1/ticker/24hr?symbol=${ticker}`))[0].weightedAvgPrice); // Use a 24hr avergae instead of spot because BNC doesn't have much volume and this should make price manipulation harder
 
   const tickerGASPrice = 1 / (tickerPrice * medianPriceBNBGAS);
 
@@ -50,4 +54,4 @@ async function updateOracle() {
   await Neon.doInvoke(config);
 }
 
-module.exports = { updateOracle, getPrice };
+module.exports = { updateOracle, getPrice, getBNBGASPrice };
--- updateOracle.test.js
+++ updateOracle.test.js
@@ -1,8 +1,17 @@
-const { getPrice } = require('./updateOracle');
+const { getPrice, getBNBGASPrice } = require('./updateOracle');
 
 test('getPrice returns an integer', async () => {
   jest.setTimeout(30000);
   const price = await getPrice('BNB_BTCB-1DE'); // Ticker for BTC on Binance Chain
   expect(typeof price).toBe('number');
   expect(price % 1).toBe(0);
+});
+
+test('getBNBGASPrice returns similar values', async () => {
+  jest.setTimeout(30000);
+  const price = await getBNBGASPrice();
+  price.forEach((p, i) => {
+    const precision = i === 3 ? 1 : 2; // HitBTC is less precise, don't enforce high precision on that one
+    expect(price[0]).toBeCloseTo(p, precision); // All values should be similar
+  });
 });
